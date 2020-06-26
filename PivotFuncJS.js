d71e0a7f16eec96f722323b16fdd7d23cd00f227
zycooper <yuanzhang998@gmail.com>
2020-06-26 14:46:28 -0500

    Update PivotFuncJS.js

--- PivotFuncJS.js
+++ PivotFuncJS.js
@@ -1,9 +1,34 @@
-<script>
-  var sourceData=[];
-</script>
 
+<html>
+<p>Pivot Table</p>
+<div id="pivot_tbl"><div>
+</html>
 
 <script>
+  Array.prototype.sum = function (prop) {
+        var result = 0
+        for ( var i = 0, _len = this.length; i < _len; i++ ) {
+          if(!isNaN(this[i][prop]))
+          {
+            result += this[i][prop]
+          }
+        }
+        return result;
+    }
+
+  Array.prototype.avg = function (prop) {
+        var result = 0
+        for ( var i = 0, _len = this.length; i < _len; i++ ) {
+          if(!isNaN(this[i][prop]))
+          {
+            result += this[i][prop]
+          }
+        }
+        if(this.length>0)
+        {return (result / this.length).toFixed(2)}
+        else
+        {return 0;}
+    }   
   /*  
     Cal Func Code: 
     0. Count
@@ -13,7 +38,7 @@
     4. Min
   */
 
-function Pivot(rawData,columnX,columnY_List,sortX_Asc,sortY_Asc,Calfunc,CalCol)
+function Pivot(rawData,columnX,columnY_List,sortX_Asc,sortY_Asc,isTotalLeft,Calfunc,CalCol)
 {
   //convert json to Array: [0] is the column array, from [1] is the data -result is the converted data array
 
@@ -62,28 +87,27 @@ function Pivot(rawData,columnX,columnY_List,sortX_Asc,sortY_Asc,Calfunc,CalCol)
   //add first row - Columns
   let FirstRow = [];
   columnY_List.forEach(x => {FirstRow.push(x);})
-
-  //FirstRow.push("Total"); add later
   columnXValue.forEach(x => {FirstRow.push(x);})
+
+  isTotalLeft? FirstRow.splice(columnY_List.length,0,"Total") : FirstRow.push("Total");
+
   Result.push(FirstRow);
 
   //add value rows
-
   //set the y-axis pivot values
   let pivot_list=Axis_Value_List(Y_axis_Pairs,0); 
-
-  for(let i=0; i < pivot_list.length; i++)
+  
+  //add cell values then add rows
+  for(let i = 0; i < pivot_list.length; i++)
   {
     let CellRow=[];
+    let TotalCell=0;//the total cell value
 
     //push axis values
     for(let j = 0; j < pivot_list[i].length; j++)
     {
       CellRow.push(pivot_list[i][j]);
     }
-
-    //push total numbers add later
-    //CellRow.push(null); ///////not valid yet -- calculate later base on all other cell value
 
     //push cell value  
     for(let k = 0; k < columnXValue.length; k++)
@@ -122,31 +146,89 @@ function Pivot(rawData,columnX,columnY_List,sortX_Asc,sortY_Asc,Calfunc,CalCol)
               4. Min
             */
           case 0:
-          Cal_result = rows.length;
+            Cal_result = rows.length;
+            TotalCell +=Cal_result;
           break;
           case 1:
+            if(CalCol != "" && CalCol != undefined)
+            {
+              Cal_result = rows.sum(CalCol);
+              TotalCell +=Cal_result;
+            }
           break;
           case 2:
+            if(CalCol != "" && CalCol != undefined)
+            {
+              Cal_result = rows.avg(CalCol);
+            }
           break;
           case 3:
+            if(CalCol != "" && CalCol != undefined)
+            {
+              Cal_result = Math.max.apply(Math,rows.map(function(o){ return o[CalCol];}))
+            }
           break;
           case 4:
+            if(CalCol != "" && CalCol != undefined)
+            {
+              Cal_result = Math.min.apply(Math,rows.map(function(o){ return o[CalCol];}))
+            }
           break;
         }
       }  
-
       CellRow.push(Cal_result);
-
-      Result.push(CellRow);
     }
-        
-   
-    //add total cell
-  }  
+
+    //push total value to the row base on where the total will be
+    isTotalLeft? CellRow.splice(pivot_list[0].length,0,TotalCell) : CellRow.push(TotalCell);
+    //CellRow.splice((isTotalLeft ? pivot_list[0].length : (pivot_list[0].length + columnXValue.length-1)),0,TotalCell);    
+    Result.push(CellRow);
+  }
+  
+  //add total last row
+  let TotalRow =[];
+  for(let i=0; i< Y_axis_Pairs.length-1;i++){TotalRow.push("-");/*null values to fill the bottom cell under y-axis-column value*/}
+  TotalRow.push("Total");
+  
+  //fill total row
+  for(let i =Y_axis_Pairs.length;i < Result[0].length;i++)
+  {
+    let total_result =0;
+    
+    for(let j = 1 ; j < Result.length;j++)
+    {
+      //let testoneeee=Result[j][i];
+      
+      total_result += ((isNaN(Result[j][i]))? 0 : Result[j][i]);
+    }
+
+    TotalRow.push(total_result);
+  }
+
+  Result.push(TotalRow);
   return Result;
 }
-let pivot_tbl = Pivot(sourceData,"",["",""],false,false);
+let pivot_tbl = Pivot(sourceData,"Aging_By_Received",["Status"],true,false,false,0);
 
+RenderPivotTbl(pivot_tbl);
+
+function RenderPivotTbl(tbl_array)
+{
+  var html = "<table border='1|1'>";
+  for (var i = 0; i < tbl_array.length; i++) {
+      html+="<tr>";
+
+      for(var j=0; j < tbl_array[i].length;j++)
+      {
+        html+="<td>"+tbl_array[i][j]+"</td>";    
+      }
+
+      html+="</tr>";
+
+  }
+  html+="</table>";
+  document.getElementById("pivot_tbl").innerHTML = html;
+}
 function SortDistinctValue(sort,_array)
 {
   if(!_array.some(isNaN))
@@ -227,7 +309,3 @@ function DeepCopy(source)
   return result;
 }
 </script>
-
-<html>
-<p>test</p>
-</html>
