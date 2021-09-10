a5c3c41db8b7068e229a88d63603ed89ea11decf
Yuan Zhang <yuanzhang998@gmail.com>
2021-09-09 20:23:30 -0500

    Update Pivot.cs

--- .net/Pivot.cs
+++ .net/Pivot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace SMS_Dev_Tool
+namespace Tool
 {
     public enum PivotFuncName { Count, Sum, Avg, Min, Max, CountDistinct }
     public class PivotFunc
