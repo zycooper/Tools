eb14761a6729d5ea592dab849065969a7e997225
Yuan Zhang <yuanzhang998@gmail.com>
2021-09-09 20:23:16 -0500

    Update EmailSend.cs

--- .net/EmailSend.cs
+++ .net/EmailSend.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace SMS_Dev_Tool
+namespace Tool
 {
     //send Email
     public class EmailSourceUnit
